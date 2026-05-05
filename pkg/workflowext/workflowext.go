a18cdc981c5e6c142bcf60e54d8ab8e524ef6312
David Wrenner <davidwrenner@gmail.com>
2026-05-05 14:05:17 -0700

    feat: update lib with generics

--- pkg/workflowext/workflowext.go
+++ pkg/workflowext/workflowext.go
@@ -8,17 +8,17 @@ import (
 
 // NoOpActivity is a traceable activity that does nothing.
 // It is exported so it can be registered with the Temporal worker.
-func NoOpActivity(_ context.Context, _ *any) error {
+func NoOpActivity(_ context.Context) error {
 	return nil
 }
 
 // ExecuteOptional wraps workflow.ExecuteActivity.
-// If args is nil, it executes a NoOpActivity to maintain a visible execution history.
-// If args is non-nil, it proceeds with the provided activity and arguments.
-func ExecuteOptional(ctx workflow.Context, activity any, args *any) workflow.Future {
-	if args == nil {
+// If input is nil, it executes a NoOpActivity to maintain a visible execution history.
+// If input is non-nil, it proceeds with the provided activity and input.
+func ExecuteOptional[S, T any](ctx workflow.Context, activity S, input *T) workflow.Future {
+	if input == nil {
 		return workflow.ExecuteActivity(ctx, NoOpActivity)
 	}
 
-	return workflow.ExecuteActivity(ctx, activity, args)
+	return workflow.ExecuteActivity(ctx, activity, input)
 }
